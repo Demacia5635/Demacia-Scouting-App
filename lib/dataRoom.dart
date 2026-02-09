8d2e013546f041cae7b5b04ee55103e89cdfa790
matan515 <diz.matan@gmail.com>
2026-02-09 20:37:30 +0200

    i crate the window

--- lib/dataRoom.dart
+++ lib/dataRoom.dart
@@ -1,3 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class dataRoom {}
+void main() {
+  runApp(dataRoom());
+}
+
+class dataRoom extends StatefulWidget {
+  const dataRoom({super.key});
+
+  @override
+  State<dataRoom> createState() => dataRoomState();
+}
+
+class dataRoomState extends State<dataRoom> {
+  final SupabaseClient _supabase = Supabase.instance.client;
+
+  List<dynamic> dataList = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    myMethod();
+  }
+
+  Future<void> myMethod() async {
+    final data = await _supabase.from('data').select();
+
+    setState(() {
+      dataList = data;
+      isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Data Room')),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: dataList.length,
+              itemBuilder: (context, index) {
+                return ListTile(title: Text('${dataList[index]}'));
+              },
+            ),
+    );
+  }
+}
