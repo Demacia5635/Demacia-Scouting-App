2a93800be17b9f18498f84c82a9670024c441b06
fastMirta <tamir.szewach@gmail.com>
2026-02-19 23:08:22 +0200

    styled the scanner to look like app

--- lib/widgets/qr_code.dart
+++ lib/widgets/qr_code.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:qr_code_scanner_plus/qr_code_scanner_plus.dart';
 
 void main() {
-  runApp(MaterialApp(home: QRCode()));
+  runApp(MaterialApp(home: QRCode(), debugShowCheckedModeBanner: false));
 }
 
 class QRCode extends StatefulWidget {
@@ -18,6 +18,7 @@ class QRCodeState extends State<QRCode> {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   Barcode? resolt;
   QRViewController? controller;
+  bool isScanMode = false;
 
   @override
   void reassemble() {
@@ -44,12 +45,27 @@ class QRCodeState extends State<QRCode> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('qr code')),
+      appBar: AppBar(
+        title: const Text('QR Code Scanner'),
+        backgroundColor: Colors.deepPurple.shade700,
+      ),
+      backgroundColor: Colors.deepPurple.shade700,
       body: Column(
         children: [
-          Expanded(
-            flex: 5,
-            child: QRView(key: qrKey, onQRViewCreated: _onQRViewCreated),
+          isScanMode
+              ? Expanded(
+                  flex: 5,
+                  child: QRView(key: qrKey, onQRViewCreated: _onQRViewCreated),
+                )
+              : Spacer(flex: 3),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                isScanMode = !isScanMode;
+              });
+            },
+            icon: Icon(Icons.add_a_photo, color: Colors.black),
+            iconSize: 100,
           ),
           Expanded(
             flex: 5,
