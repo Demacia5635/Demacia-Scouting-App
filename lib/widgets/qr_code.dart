24d5ab3f2a47e7368a2b43e5e6ca6c57493ae692
ben_banana555 <ben1.althsul@gmail.com>
2026-02-19 18:35:51 +0200

    i cant run at no at the phone

--- lib/widgets/qr_code.dart
+++ lib/widgets/qr_code.dart
@@ -2,9 +2,14 @@
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
+
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner_plus/qr_code_scanner_plus.dart';
 
+
+void main(){
+  runApp(MaterialApp(home: qr_code()));
+}
 
 class qr_code extends StatefulWidget {
   @override
